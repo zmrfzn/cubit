ac9a6b692e71b524e1f441f8b9a34292d17eadcc
zameer <zmrfzn@gmail.com>
2016-07-17 02:46:36 +0530

    functioning, all values processed to DB

--- trips.py
+++ trips.py
@@ -1,25 +1,30 @@
 from mysql import connector
-import mysql.connector.errors
+import mysql.connector.errors as CE
 from urllib2 import urlopen
+from multiprocessing import pool
 import json
 
-trip_ids=[]
 #db connection object
 conn = connector.connect(host='localhost',user='root',passwd='root',db='cubito')
 cursor = conn.cursor()
 
 
 def getDetail(trip_id,current_state):
+	
+	location = []
 	while (current_state=="RUNNING"):
 		trip_detail = json.load(urlopen("http://cubito.co.in/assignment/gpslocation.php?trip_id="+trip_id))
 		print trip_detail["status"]+":"+trip_detail["trip_id"]
+		
+		location.append(str(trip_detail["location"]["latitude"])+","+str(trip_detail["location"]["longitude"]))
+		
 		#Fail Safe
 		if trip_detail["status"] == "COMPLETED":
 			current_state="COMPLETED"
+			json_loc= json.dumps(location)
 			break
 		
-	return
-
+	return json_loc
 #get trip ids
 def getTripData():
 	flag=1
@@ -29,8 +34,15 @@ def getTripData():
 		print str(trip_id["trip_id"])+"START"
 		dummy = json.load(urlopen("http://cubito.co.in/assignment/gpslocation.php?trip_id="+trip_id["trip_id"]))
 		current_state = dummy["status"]
-		getDetail(trip_id["trip_id"],current_state)
-		print str(trip_id["trip_id"])+"STOP"
+		location = getDetail(trip_id["trip_id"],current_state)
+		print location
+		try:
+			cursor.execute("insert into trips values('"+trip_id["trip_id"]+"','"+location+"')")
+			conn.commit()
+		except (CE.Error ,Exception) as e:
+			print e
 		
+		print str(trip_id["trip_id"])+"STOP"
+
 
 getTripData()
